82aa3618cce0512276de5d51081f8061b827e777
devlexanderxyz <infinityen@protonmail.com>
2020-09-19 19:57:54 +0100

    complete project restructure

---
+++ sampgdk/build/obj/Debug/amxplugin.d
@@ -0,0 +1,7 @@
+obj/Debug/amxplugin.o: ../amxplugin.c ../plugin.h ../plugincommon.h \
+ ../amx/amx.h ../amx/sclinux.h ../amx/getch.h
+../plugin.h:
+../plugincommon.h:
+../amx/amx.h:
+../amx/sclinux.h:
+../amx/getch.h:
