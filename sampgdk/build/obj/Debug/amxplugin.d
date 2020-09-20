8bad60d45c673adc9c59f90d5f3460f8b847917c
devlexanderxyz <infinityen@protonmail.com>
2020-09-20 11:50:23 +0100

    go back to new linker paths

--- sampgdk/build/obj/Debug/amxplugin.d
+++
@@ -1,7 +0,0 @@
-obj/Debug/amxplugin.o: ../amxplugin.c ../plugin.h ../plugincommon.h \
- ../amx/amx.h ../amx/sclinux.h ../amx/getch.h
-../plugin.h:
-../plugincommon.h:
-../amx/amx.h:
-../amx/sclinux.h:
-../amx/getch.h:
--- sampgdk/build/obj/Debug/main.d
+++
@@ -1,8 +0,0 @@
-obj/Debug/main.o: ../main.c ../main.h ../sampgdk.h ../amx/amx.h \
- ../amx/sclinux.h ../amx/getch.h ../plugincommon.h
-../main.h:
-../sampgdk.h:
-../amx/amx.h:
-../amx/sclinux.h:
-../amx/getch.h:
-../plugincommon.h:
