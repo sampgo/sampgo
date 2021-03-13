2262ba663f3edd9e382733478ec69491636dac78
alex <38814044+alextwothousand@users.noreply.github.com>
2021-03-13 20:02:14 +0000

    dl.so not needed on win

--- sampgo.go
+++ sampgo.go
@@ -6,7 +6,6 @@ package sampgo
 
 #cgo windows CFLAGS: -I./sampgdk -I./sampgdk/amx -DWIN32 -D_GNU_SOURCE -Wno-implicit-function-declaration -Wno-attributes -DSAMPGDK_GOLANG -include stdint.h
 #cgo windows CFLAGS: -DHAVE_INTTYPES_H -DHAVE_MALLOC_H -DHAVE_STDINT_H
-#cgo windows LDFLAGS: -ldl
 
 #ifndef GOLANG_APP
 #define GOLANG_APP
