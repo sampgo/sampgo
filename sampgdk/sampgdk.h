44d96a852cc2637ddfb428fe25478bc81b0f963c
alex <38814044+alextwothousand@users.noreply.github.com>
2021-03-20 12:04:52 +0000

    apply cleaner calling conventions fix
    from zeex/sampgdk master branch

--- sampgdk/sampgdk.h
+++ sampgdk/sampgdk.h
@@ -76,15 +76,13 @@ typedef int sizeof_bool_must_be_1[sizeof(bool) == 1 ? 1 : -1];
 
 // previous sampgdk macros.
 #if SAMPGDK_WINDOWS
-	#ifdef _MSC_VER
-		#define SAMPGDK_CDECL __cdecl
-		#define SAMPGDK_STDCALL __stdcall
-	#else
-		// sampgo related changes.
-		// we are still using gcc, regardless whether its on linux or windows. we need to fix calling conventions.
-		#define SAMPGDK_CDECL __attribute__((cdecl))
-        #define SAMPGDK_STDCALL __attribute__((stdcall))
-	#endif
+  #ifdef _MSC_VER
+    #define SAMPGDK_CDECL __cdecl
+    #define SAMPGDK_STDCALL __stdcall
+  #else
+    #define SAMPGDK_CDECL __attribute__((cdecl))
+    #define SAMPGDK_STDCALL __attribute__((stdcall))
+  #endif
 #elif SAMPGDK_LINUX
 #define SAMPGDK_CDECL __attribute__((cdecl))
 #define SAMPGDK_STDCALL __attribute__((stdcall))
