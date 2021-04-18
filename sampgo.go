8688a04ab99dbcac04ff26ab1bd1335c02982dc3
AliLogic <drfiniz@gmail.com>
2021-04-18 16:02:06 +0500

    Update: Windows LDFlags

--- sampgo.go
+++ sampgo.go
@@ -3,7 +3,7 @@ package sampgo
 /*
 #cgo windows CFLAGS: -I./lib -I./lib/amx -Wno-attributes -Wno-implicit-function-declaration
 #cgo windows CFLAGS: -DHAVE_INTTYPES_H -DHAVE_MALLOC_H -DHAVE_STDINT_H -DWIN32
-#cgo windows LDFLAGS: -Wl,--subsystem,windows
+#cgo windows LDFLAGS: -Wl,--subsystem,windows,--kill-at
 
 #cgo linux CFLAGS: -I./lib -I./lib/amx -Wno-attributes -Wno-implicit-function-declaration
 #cgo linux CFLAGS: -DHAVE_INTTYPES_H -DHAVE_MALLOC_H -DHAVE_STDINT_H -DLINUX
