d4a4ca0a33701be4e7d1cff1231a81129fe6632e
AliLogic <drfiniz@gmail.com>
2021-05-10 12:56:26 +0500

    Remove: native function header

--- lib/main.h
+++ lib/main.h
@@ -104,8 +104,6 @@ extern bool onPlayerRequestDownload(int playerid, int type, int crc);
 extern void onTick();
 #endif
 
-cell n_CallEvent(AMX* amx, cell* params);
-
 // All of the natives we want to export.
 extern void goLogprintf(char* text);
 extern char* constToNonConst(const char* text);
